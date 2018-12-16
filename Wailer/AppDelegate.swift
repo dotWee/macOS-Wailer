1d40ca929cab04a1a24126c93b1d1334aba12377
Lukas Wolfsteiner <lukas@wolfsteiner.media>
2018-12-16 02:13:30 +0100

    Initial Commit

---
+++ Wailer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Wailer
+//
+//  Created by Lukas Wolfsteiner on 16.12.18.
+//  Copyright © 2018 Lukas Wolfsteiner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Wailer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Wailer
+//
+//  Created by Lukas Wolfsteiner on 16.12.18.
+//  Copyright © 2018 Lukas Wolfsteiner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
