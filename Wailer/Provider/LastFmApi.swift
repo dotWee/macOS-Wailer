bd0193a177a21023feb7eaf9f27b87f2b7a6aa6b
Lukas Wolfsteiner <lukas@wolfsteiner.media>
2018-12-20 15:09:20 +0100

    Define scrobbler protocol with Last.fm and Libre.fm implementation

--- Wailer/Provider/LastFmApi.swift
+++ Wailer/Provider/LastFmApi.swift
@@ -10,13 +10,17 @@ import Cocoa
 import Alamofire
 import CommonCrypto
 
-class LastFmApi: NSObject {
-
-    static var URL_BASE = "http://ws.audioscrobbler.com/2.0"
-    static var URL_GET_AUTH_TOKEN = URL_BASE + "/?method=auth.gettoken&api_key=" + Constants.API_KEY + "&format=json"
-    static var URL_USER_PERFORM_AUTH = "http://www.last.fm/api/auth/?api_key=" + Constants.API_KEY + "&token="
+class LastFmApi: ScrobblerApiProtocol {
     
-    static func getMethodUrl(method: String, parameters: [NSURLQueryItem]? = nil) -> URL {
+    static var name: String = "Last.fm"
+    static var urlHandshake: String = "http://post.audioscrobbler.com/?hs=true"
+    static var urlSignUp: String = "https://www.last.fm/join"
+    static var urlProfile: String = "https://www.last.fm/user/"
+    static var urlWebservice: String = "https://ws.audioscrobbler.com/2.0"
+    
+    static var urlAuth: String = "http://www.last.fm/api/auth/?api_key=" + Constants.API_KEY + "&token="
+    
+    static func getMethodUrl(method: String, parameters: [NSURLQueryItem]?) -> URL {
         var queryItems = [NSURLQueryItem(name: "method", value: method),
                           NSURLQueryItem(name: "api_key", value: Constants.API_KEY),
                           ]
@@ -27,61 +31,23 @@ class LastFmApi: NSObject {
         let signature = getSignature(queryParams: queryItems)
         queryItems.append(NSURLQueryItem(name: "api_sig", value: signature))
         queryItems.append(NSURLQueryItem(name: "format", value: "json"))
-
-        let urlComps = NSURLComponents(string: URL_BASE)!
+        
+        let urlComps = NSURLComponents(string: LastFmApi.urlWebservice)!
         urlComps.queryItems = queryItems as [URLQueryItem]
         
         return urlComps.url!
     }
-
-    public static func getAuthToken() -> DataRequest {
-        let URL = getMethodUrl(method: "auth.gettoken");
+    
+    func getAuthToken() -> DataRequest {
+        let URL = LastFmApi.getMethodUrl(method: "auth.gettoken", parameters: nil);
         
         return Alamofire.request(URL)
     }
     
-    static func getSignature(queryParams: [NSURLQueryItem]) -> String! {
-        var paramsDict: [String: String] = [:]
-        queryParams.forEach { (queryParam) in
-            paramsDict[queryParam.name] = queryParam.value!
-        }
-        
-        var signature = ""
-        paramsDict.keys.sorted().forEach { (key) in
-            let value = paramsDict[key]
-            signature.append(key)
-            signature.append(value!)
-        }
-        
-        print("Signature: \(signature)")
-        signature.append(Constants.API_SECRET)
-        return md5(signature)!
-    }
-    
-    static func md5(_ string: String) -> String? {
-        let length = Int(CC_MD5_DIGEST_LENGTH)
-        var digest = [UInt8](repeating: 0, count: length)
-        
-        if let d = string.data(using: String.Encoding.utf8) {
-            _ = d.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
-                CC_MD5(body, CC_LONG(d.count), &digest)
-            }
-        }
-        
-        return (0..<length).reduce("") {
-            $0 + String(format: "%02x", digest[$1])
-        }
-    }
-    
-    public static func getSession(token: String) -> DataRequest {
-        let URL = getMethodUrl(method: "auth.getSession", parameters: [NSURLQueryItem(name: "token", value: token)])
+    func getSession(token: String) -> DataRequest {
+        let URL = LastFmApi.getMethodUrl(method: "auth.getSession", parameters: [NSURLQueryItem(name: "token", value: token)])
         print("URL: \(URL)")
         
         return Alamofire.request(URL)
-    }
-    
-    public static func openAuthUrl(token: String) {
-        let url = URL_USER_PERFORM_AUTH + token
-        NSWorkspace.shared.open(URL.init(string: url)!)
     }
 }
---
+++ Wailer/Provider/ScrobblerProtocol.swift
@@ -0,0 +1,124 @@
+//
+//  ScrobblerProtocol.swift
+//  Wailer
+//
+//  Created by Lukas Wolfsteiner on 18.12.18.
+//  Copyright © 2018 Lukas Wolfsteiner. All rights reserved.
+//
+
+import Cocoa
+import Alamofire
+import CommonCrypto
+
+protocol ScrobblerApiProtocol {
+    static var name: String { get }
+    
+    static var urlAuth: String { get }
+    static var urlHandshake: String { get }
+    static var urlSignUp: String { get }
+    static var urlProfile: String { get }
+    static var urlWebservice: String { get }
+    
+    static func getMethodUrl(method: String, parameters: [NSURLQueryItem]?) -> URL
+    func getAuthToken() -> DataRequest
+    static func getSignature(queryParams: [NSURLQueryItem]) -> String!
+    static func md5(_ string: String) -> String?
+    func getSession(token: String) -> DataRequest
+    static func openAuthUrl(token: String)
+}
+
+protocol ScrobblerApiTokenHandshakeProtocol {
+    func onSuccess(token: String)
+
+    func onError(code: Int, message: String)
+    func onError(result: Result<Any>)
+}
+
+extension ScrobblerApiProtocol {
+    static func openAuthUrl(token: String) {
+        let url = self.urlAuth + token
+        NSWorkspace.shared.open(URL.init(string: url)!)
+    }
+    
+    static func md5(_ string: String) -> String? {
+        let length = Int(CC_MD5_DIGEST_LENGTH)
+        var digest = [UInt8](repeating: 0, count: length)
+        
+        if let d = string.data(using: String.Encoding.utf8) {
+            _ = d.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
+                CC_MD5(body, CC_LONG(d.count), &digest)
+            }
+        }
+        
+        return (0..<length).reduce("") {
+            $0 + String(format: "%02x", digest[$1])
+        }
+    }
+    
+    static func getSignature(queryParams: [NSURLQueryItem]) -> String! {
+        var paramsDict: [String: String] = [:]
+        queryParams.forEach { (queryParam) in
+            paramsDict[queryParam.name] = queryParam.value!
+        }
+        
+        var signature = ""
+        paramsDict.keys.sorted().forEach { (key) in
+            let value = paramsDict[key]
+            signature.append(key)
+            signature.append(value!)
+        }
+        
+        print("Signature: \(signature)")
+        signature.append(Constants.API_SECRET)
+        return md5(signature)!
+    }
+    
+    func performTokenRequest(handshakeProtocol: ScrobblerApiTokenHandshakeProtocol) {
+        
+        self.getAuthToken().responseJSON { response in
+            let json = response.result.value as! [String: AnyObject?]
+            if let token = json["token"] as! String? {
+                handshakeProtocol.onSuccess(token: token)
+                
+            } else if let code = json["error"] as! Int? {
+                let message = json["message"] as! String
+                handshakeProtocol.onError(code: code, message: message)
+                
+            } else {
+                handshakeProtocol.onError(result: response.result)
+            }
+        }
+    }
+    
+    func performSessionRequest(handshakeProtocol: ScrobblerApiSessionHandshakeProtocol, token: String) {
+        
+        self.getSession(token: token).responseJSON { response in
+            let json = response.result.value as! [String: AnyObject?]
+            print("json=\(json)")
+            
+            if let session = json["session"] as AnyObject? {
+                let sessionKey = session["key"] as! String
+                let sessionName = session["name"] as! String
+                let sessionSubscriber = session["subscriber"] as! Int
+                
+                handshakeProtocol.onSuccess(key: sessionKey, name: sessionName, subscriber: sessionSubscriber)
+                
+            } else if let code = json["error"] as! Int? {
+                let message = json["message"] as! String
+                handshakeProtocol.onError(code: code, message: message)
+                
+            } else {
+                handshakeProtocol.onError(result: response.result)
+                
+            }
+            
+            return
+        }
+    }
+}
+
+protocol ScrobblerApiSessionHandshakeProtocol {
+    func onSuccess(key: String, name: String, subscriber: Int);
+    func onError(code: Int, message: String)
+    func onError(result: Result<Any>)
+}
